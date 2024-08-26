716f81dd3490869ba0e863b47bf561bee45f71e7
Nexalyze <sourishchatterjee0067@gmail.com>
2024-08-26 15:53:18 +0530

    File Changes

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
-
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('light-mode');
-    toggleButton.textContent = body.classList.contains('light-mode') ? '🌑' : '🌒';
-});
